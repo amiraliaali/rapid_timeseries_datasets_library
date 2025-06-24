de76fd3c8e02a839c9f8e4aaf22cd6c212abe914
Marius Kaufmann <marius.kaufmann@rwth-aachen.de>
2025-06-24 21:55:22 +0200

    adapt wrapper

--- source_code/python/wrapper.py
+++ source_code/python/wrapper.py
@@ -1,14 +1,21 @@
+from enum import Enum
 import pytorch_lightning as L
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset
 import numpy as np
 from rust_time_series.rust_time_series import (
-    BaseDataSet,
-    DatasetType,
+    ForecastingDataSet,
+    ClassificationDataSet,
     ImputeStrategy,
     SplittingStrategy,
 )
 
-print ("Rust Time Series Wrapper Loaded")
+print("Rust Time Series Wrapper Loaded")
+
+
+class DatasetType(Enum):
+    Forecasting = "forecasting"
+    Classification = "classification"
+
 
 class RustDataModule(L.LightningDataModule):
     def __init__(
@@ -16,11 +23,12 @@ class RustDataModule(L.LightningDataModule):
         dataset: np.ndarray,
         dataset_type: DatasetType,
         past_window: int = 1,
-        future_window: int = 1,
+        future_horizon: int = 1,
         stride: int = 1,
-        labels: np.ndarray = None,
+        labels: np.ndarray | None = None,
         batch_size: int = 32,
         num_workers: int = 0,
+        downsampling_rate: int = 0,
         normalize: bool = False,
         standardize: bool = False,
         impute_strategy: ImputeStrategy = ImputeStrategy.LeaveNaN,
@@ -30,14 +38,13 @@ class RustDataModule(L.LightningDataModule):
         super().__init__()
 
         self.dataset = dataset
-
         self.labels = labels
-
         self.dataset_type = dataset_type
 
         self.batch_size = batch_size
         self.num_workers = num_workers
 
+        self.downsampling_rate = downsampling_rate
         self.normalize = normalize
         self.standardize = standardize
 
@@ -47,35 +54,55 @@ class RustDataModule(L.LightningDataModule):
         self.splitting_ratios = splitting_ratios
 
         self.past_window = past_window
-        self.future_window = future_window
+        self.future_horizon = future_horizon
         self.stride = stride
 
-    def setup(self):
+    def setup(self, stage: str):
         if self.dataset_type == DatasetType.Forecasting:
             # call the method that applies the preprocessing steps and returns the split datasets
-            ts = BaseDataSet(self.dataset, 
-                                    self.past_window, self.future_window, self.stride)
-        elif self.dataset_type == DatasetType.Classification:
-            ts = BaseDataSet.new_classification(
-                self.dataset, self.labels,
-            )
-
-        # Apply normalization if specified
-        if self.normalize:
-            ts.normalize()
-
-        # Apply standardization if specified
-        if self.standardize:
-            ts.standardize()
+            dataset = ForecastingDataSet(self.dataset)
+        else:
+            dataset = ClassificationDataSet(self.dataset, self.labels)
 
         # Apply imputation strategy if specified
         if self.impute_strategy != ImputeStrategy.LeaveNaN:
-            ts.impute(self.impute_strategy)
+            dataset.impute(self.impute_strategy)
 
-        # Split the dataset into train, validation, and test sets
-        (self.train_data, self.val_data, self.test_data) = ts.split(
-            self.splitting_strategy, *self.splitting_ratios
+        # Apply downsampling if specified
+        if self.downsampling_rate > 0:
+            dataset.downsample(self.downsampling_rate)
+
+        # Split the data
+        split_args = (
+            (self.splitting_strategy, *self.splitting_ratios)
+            if self.dataset_type == DatasetType.Classification
+            else self.splitting_ratios
         )
+        dataset.split(*split_args)
+
+        # Apply normalization if specified
+        if self.normalize:
+            dataset.normalize()
+
+        # Apply standardization if specified
+        if self.standardize:
+            dataset.standardize()
+
+        # Collect the results
+        collect_args = (
+            (self.past_window, self.future_horizon, self.stride)
+            if self.dataset_type == DatasetType.Forecasting
+            else ()
+        )
+        (
+            (X_train, y_train),
+            (X_val, y_val),
+            (X_test, y_test),
+        ) = dataset.collect(*collect_args)
+
+        self.train_data = TensorDataset(X_train, y_train)
+        self.val_data = TensorDataset(X_val, y_val)
+        self.test_data = TensorDataset(X_test, y_test)
 
     def train_dataloader(self):
         # Return the training dataloader
