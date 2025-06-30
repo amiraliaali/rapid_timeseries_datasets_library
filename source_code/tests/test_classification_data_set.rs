7278600cadfc7c752913793dc306df87858e8439
Amirali Aali <amiraliaali@gmail.com>
2025-06-30 23:16:10 +0200

    Modifying the first test

--- source_code/tests/test_classification_data_set.rs
+++ source_code/tests/test_classification_data_set.rs
@@ -1,13 +1,16 @@
 #[cfg(test)]
 mod tests {
     use pyo3::prelude::*;
+    use numpy::{PyArray1, PyArray3, IntoPyArray};
+    use ndarray::Array;
+    use pyo3::types::PyTuple;
 
     #[test]
-    fn test_py_function() {
+    fn test_importing_module() {
         pyo3::prepare_freethreaded_python();
         Python::with_gil(|py| {
             let module = py.import("rust_time_series");
-            assert!(module.is_ok()); // better than unwrap in test
+            assert!(module.is_ok());
         });
     }
 }
