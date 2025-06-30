a0b72b9bfb193682069066a9a18f07c7f5d1c89a
Amirali Aali <amiraliaali@gmail.com>
2025-06-30 23:06:41 +0200

    Making the tests to run

--- source_code/tests/tests.rs
+++ source_code/tests/tests.rs
@@ -6,8 +6,8 @@ mod tests {
     fn test_py_function() {
         pyo3::prepare_freethreaded_python();
         Python::with_gil(|py| {
-            let module = py.import("rust_time_series").unwrap();
-            assert_eq!(2, 3);
+            let module = py.import("rust_time_series");
+            assert!(module.is_ok()); // better than unwrap in test
         });
     }
 }
