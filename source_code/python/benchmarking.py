4d67e72399a51a162e2f0a5690e054a1b8d6c36e
kilbraun <kilian.braun@rwth-aachen.de>
2025-07-13 13:20:08 +0200

    wip

--- source_code/python/benchmarking.py
+++ source_code/python/benchmarking.py
@@ -10,6 +10,144 @@ from rust_time_series.rust_time_series import (
     SplittingStrategy,
 )
 
+
+from enum import Enum
+import pytorch_lightning as L
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import numpy as np
+from rust_time_series.rust_time_series import (
+    ForecastingDataSet,
+    ClassificationDataSet,
+    ImputeStrategy,
+    SplittingStrategy,
+)
+import time
+print("Rust Time Series Wrapper Loaded")
+
+
+class DatasetType(Enum):
+    Forecasting = "forecasting"
+    Classification = "classification"
+
+
+class BenchmarkingModule(wrapper.RustDataModule):
+    def __init__(
+        self,
+        dataset: np.ndarray,
+        dataset_type: DatasetType,
+        past_window: int = 1,
+        future_horizon: int = 1,
+        stride: int = 1,
+        labels: np.ndarray | None = None,
+        batch_size: int = 32,
+        num_workers: int = 0,
+        downsampling_rate: int = 0,
+        normalize: bool = False,
+        standardize: bool = False,
+        impute_strategy: ImputeStrategy = ImputeStrategy.LeaveNaN,
+        splitting_strategy: SplittingStrategy = SplittingStrategy.Temporal,
+        splitting_ratios: tuple = (0.7, 0.2, 0.1),  # Train, validation, test ratios
+    ):
+        super().__init__(
+            dataset,
+            dataset_type,
+            past_window,
+            future_horizon,
+            stride,
+            labels,
+            batch_size,
+            num_workers,
+            downsampling_rate,
+            normalize,
+            standardize,
+            impute_strategy,
+            splitting_strategy,
+            splitting_ratios
+        )
+
+        self.timings = {}
+
+    def setup(self, stage: str):
+        if self.dataset_type == DatasetType.Forecasting:
+            # call the method that applies the preprocessing steps and returns the split datasets
+            dataset = ForecastingDataSet(self.dataset)
+        else:
+            dataset = ClassificationDataSet(self.dataset, self.labels)
+
+        # Apply imputation strategy if specified
+        if self.impute_strategy != ImputeStrategy.LeaveNaN:
+            dataset.impute(self.impute_strategy)
+
+        # Apply downsampling if specified
+        if self.downsampling_rate > 0:
+            timer = time.time()
+            dataset.downsample(self.downsampling_rate)
+            print(f"Downsampling took {time.time() - timer:.2f} seconds")
+
+        # Split the data
+        split_args = (
+            (self.splitting_strategy, *self.splitting_ratios)
+            if self.dataset_type == DatasetType.Classification
+            else self.splitting_ratios
+        )
+        timer = time.time()
+        dataset.split(*split_args)
+        print(f"Splitting took {time.time() - timer:.2f} seconds")
+
+        # Apply normalization if specified
+        if self.normalize:
+            timer = time.time()
+            dataset.normalize()
+            print(f"Normalization took {time.time() - timer:.2f} seconds")
+
+        # Apply standardization if specified
+        if self.standardize:
+            dataset.standardize()
+
+        timer = time.time()
+        # Collect the results
+        collect_args = (
+            (self.past_window, self.future_horizon, self.stride)
+            if self.dataset_type == DatasetType.Forecasting
+            else ()
+        )
+        (
+            (X_train, y_train),
+            (X_val, y_val),
+            (X_test, y_test),
+        ) = dataset.collect(*collect_args)
+        print(f"Collecting data took {time.time() - timer:.2f} seconds")
+
+        self.train_data = TensorDataset(torch.Tensor(X_train), torch.Tensor(y_train))
+        self.val_data = TensorDataset(torch.Tensor(X_val), torch.Tensor(y_val))
+        self.test_data = TensorDataset(torch.Tensor(X_test), torch.Tensor(y_test))
+
+    def train_dataloader(self):
+        # Return the training dataloader
+        if self.train_data is None:
+            raise ValueError("Training data is not set. Call setup() first.")
+        return DataLoader(
+            self.train_data, batch_size=self.batch_size, num_workers=self.num_workers
+        )
+
+    def val_dataloader(self):
+        # Return the validation dataloader
+        if self.val_data is None:
+            raise ValueError("Validation data is not set. Call setup() first.")
+        return DataLoader(
+            self.val_data, batch_size=self.batch_size, num_workers=self.num_workers
+        )
+
+    def test_dataloader(self):
+        # Return the test dataloader
+        if self.test_data is None:
+            raise ValueError("Test data is not set. Call setup() first.")
+        return DataLoader(
+            self.test_data, batch_size=self.batch_size, num_workers=self.num_workers
+        )
+
+
 # format assumed: (n_instances, n_channels, n_timepoints)
 
 def load_electricity_data() -> np.ndarray:
@@ -90,7 +228,7 @@ SERIES2VEC_METADATA = {
     "HAR":   {"Train": 7352,   "Test": 2947,  "Subject": 30,  "Length": 128,  "Channel": 9,   "Class": 6}
 }
 
-def load_series2vec_data(dataset_title,base_path="data/series2vec/") -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def load_series2vec_data(dataset_title,base_path="/home/kilian/github/ai_with_rust_final_project/source_code/python/data/series2vec/") -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Load the Series2Vec datasets (classification)\n
     Returns train_data, train_label, test_data, test_label as numpy arrays\n
@@ -106,8 +244,18 @@ def load_series2vec_data(dataset_title,base_path="data/series2vec/") -> tuple[np
     train_label = data.item().get("train_label")
     test_data = data.item().get("test_data")
     test_label = data.item().get("test_label")
+    total_data = np.concatenate((train_data, test_data), axis=0)
+    total_label = np.concatenate((train_label, test_label), axis=0)
 
-    return train_data, train_label, test_data, test_label
+    return total_data, total_label
+
+def test_torch_normalization(tensor):
+    from pytorch_forecasting.data.encoders import TorchNormalizer
+
+    normalizer = TorchNormalizer(method="standard")
+    normalizer.fit(tensor)
+    normalized = normalizer.transform(tensor)
+
 
 if __name__ == "__main__":
     d = load_electricity_data()
